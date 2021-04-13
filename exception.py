ae4147d9c78cac49c3fd2e98d2e7c9c3694e2fc8
nazmul1001 <82289030+nazmul1001@users.noreply.github.com>
2021-04-13 14:10:43 +0200

    Create exception.py

---
+++ exception.py
@@ -0,0 +1,21 @@
+a=5
+b=0
+print("Resource open")
+try:
+    k=int(input("Please Enter a number "))
+    
+    print(a/b)
+    print (k)
+
+except ZeroDivisionError as e:
+    print("You can't divide by zero.",e)
+
+except ValueError as e:
+    print("Invalid input. You entered a Letter",e)
+    
+except Exception as e:
+    print("Something went wrong",e)
+
+
+finally:
+    print("Resource closed. Bye")
